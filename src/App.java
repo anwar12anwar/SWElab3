4047cd2f8ad31299eca5d3628b6112d02b9cf59f
Your Name <you@example.com>
2024-09-17 16:06:30 +0300

    method1

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+    public void ReadeStudents(){
+        ArrayList<String>  students = new ArrayList<>();
+        students.add("Fatima");
+        students.add("Noor");
+        students.add("Zainab");
+        students.add("Naba");
+    }
+}
