3afd2045a222484735225d0728c5309563a1d26e
Your Name <you@example.com>
2024-09-17 16:17:25 +0300

    method2

--- src/App.java
+++ src/App.java
@@ -2,13 +2,20 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        ReadeStudents();
         
     }
-    public void ReadeStudents(){
+    public static void ReadeStudents(){
         ArrayList<String>  students = new ArrayList<>();
         students.add("Fatima");
         students.add("Noor");
         students.add("Zainab");
         students.add("Naba");
+        System.out.println(students);
     }
+    public void randomGroups(ArrayList<String>  students){
+
+    }
+
+
 }
